0e513e0c70aa08e72fd8c33605f92de11c904d5c
Utkarsh Mishra <um708179@gmail.com>
2026-04-18 18:03:37 +0530

    CHORE: Increased chunk size limit to silence warning

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
